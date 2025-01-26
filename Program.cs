e5aaf1a6d4c42b1dc8bfa839933068597c0c0722
chinuh037 <chinuh037@gmail.com>
2025-01-26 17:50:06 +0530

    Allowed all cors

--- Program.cs
+++ Program.cs
@@ -8,6 +8,15 @@ builder.Services.AddSingleton<IClassService, ClassService>();
 builder.Services.AddSingleton<IBookingService, BookingService>();
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()  // Allow all hosts
+                         .AllowAnyMethod()  // Allow all HTTP methods (GET, POST, etc.)
+                         .AllowAnyHeader()); // Allow all headers
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
